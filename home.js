b6449c711ab533351c3e71e6df78bff1064f96c2
Ayush Chaudhary <106233244+iamayushgit@users.noreply.github.com>
2023-04-10 17:02:35 +0545

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //burger animation
+        burger.classList.toggle('toggle')
+    });
+}
+
+navSlide();
